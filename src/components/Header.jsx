e6ce8b74e92ab5eea6cf0ec1f1d08acc46e479a3
Mohamed Moghazi <eng.moghazy@gmail.com>
2024-08-12 14:35:58 +0200

    added componets currentweather footer and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="d-flex justify-content-center align-items-center">
+      <h2>React Weather App</h2>
+    </header>
+  );
+};
+
+export default Header;
